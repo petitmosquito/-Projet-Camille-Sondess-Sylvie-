c90af96c2adb8dfa3b6913e88448846a37de4875
Frédéric NOËL <contact@fredericnoel.com>
2019-02-12 12:11:20 +0100

    MAnigifique boucle for réalisée dans un enthousiame proche de l'hystérie collective.

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+$message = "<h1>Je fais du PHP - Boucle <em>for</em></h1>";
+
+$message .= "<ul>";
+
+for ($i = 0 ; $i < 11 ; $i++) {
+    $message .= "<li>" . $i . "</li>";
+}
+
+$message .= "</ul>";
+
+echo $message;
