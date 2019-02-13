ac01df3f4d225d8891bf56c23d4572edaf1e495c
Frédéric NOËL <contact@fredericnoel.com>
2019-02-13 11:50:46 +0100

    Ajout formulaire HTML

---
+++ includes/frmInscrption.php
@@ -0,0 +1,18 @@
+<form method="post" action="index.php?page=inscription">
+    <div>
+        <label for="nom">Nom&nbsp;: </label>
+        <input type="text" id="nom" name="nom" />
+    </div>
+    <div>
+        <label for="mail">Mail&nbsp;: </label>
+        <input type="text" id="mail" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe&nbsp;: </label>
+        <input type="password" id="mdp" name="mdp" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+</form>
