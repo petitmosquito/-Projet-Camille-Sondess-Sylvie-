2636cc0dfa1dd9bb9ace103afa40297bed73538a
Frédéric NOËL <contact@fredericnoel.com>
2019-02-13 16:46:51 +0100

    Tests validité champs de formulaire

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,3 +1,45 @@
 <h1>Inscription</h1>
 <?php
-require_once "frmInscrption.php";
+
+if (isset($_POST['maurice'])) {
+    $nom = isset($_POST['nom']) ? $_POST['nom'] : "";
+    $mail = isset($_POST['mail']) ? $_POST['mail'] : "";
+    $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : "";
+
+    $erreurs = array();
+
+    if (!(mb_strlen($nom) >= 2 && ctype_alpha($nom)))
+        array_push($erreurs, "Veuillez saisir un nom correct.");
+
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+        array_push($erreurs, "Veuillez saisir une adresse mail valide.");
+
+    if (mb_strlen($mdp) < 6)
+        array_push($erreurs, "Votre mot de passe doit comporter 6 caractères minimum");
+
+    if (count($erreurs) > 0) {
+        $message = "<ul>";
+        $i = 0;
+
+        while ($i < count($erreurs)) {
+            $message .= "<li>" . $erreurs[$i] . "</li>";
+            $i++;
+        }
+
+        $message .= "</ul>";
+
+        echo $message;
+
+        include "frmInscription.php";
+    }
+
+    else {
+        echo "Insertion en BDD";
+    }
+
+
+
+} else {
+    require_once "frmInscription.php";
+}
+
