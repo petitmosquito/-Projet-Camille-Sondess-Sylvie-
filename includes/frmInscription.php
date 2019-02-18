2af4ab45cbd0cf4f7b5e858c1a20cbd0bcf7f2bb
Frédéric NOËL <contact@fredericnoel.com>
2019-02-18 10:35:54 +0100

    Ajout champ prénom

--- includes/frmInscription.php
+++ includes/frmInscription.php
@@ -1,11 +1,16 @@
 <?php
 if (!isset($nom)) $nom = "";
+if (!isset($prenom)) $prenom = "";
 if (!isset($mail)) $mail = "";
 ?>
 <form method="post" action="index.php?page=inscription">
     <div>
         <label for="nom">Nom&nbsp;: </label>
         <input type="text" id="nom" name="nom" value="<?=$nom?>" />
+    </div>
+    <div>
+        <label for="prenom">Prénom&nbsp;: </label>
+        <input type="text" id="prenom" name="prenom" value="<?=$prenom?>" />
     </div>
     <div>
         <label for="mail">Mail&nbsp;: </label>
