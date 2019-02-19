c142f974db796abf493e390da5675eb724148156
Frédéric NOËL <contact@fredericnoel.com>
2019-02-19 10:18:14 +0100

    Ajout lien vers page de login

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
             <li><a href="index.php?page=accueil">Accueil</a></li>
             <li><a href="index.php?page=produits">Produits</a></li>
             <li><a href="index.php?page=inscription">Inscription</a></li>
+            <li><a href="index.php?page=login">Login</a></li>
             <li><a href="index.php?page=contact">Contact</a></li>
         </ul>
     </nav>
