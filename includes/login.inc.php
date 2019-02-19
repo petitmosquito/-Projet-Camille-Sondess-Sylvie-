55b8ac26e5bc6215e917d76aa0099ce04e5b5bce
Frédéric NOËL <contact@fredericnoel.com>
2019-02-19 10:29:03 +0100

    Formulaire login/password rubrique "Login"

---
+++ includes/login.inc.php
@@ -0,0 +1,41 @@
+<h1>Login</h1>
+<?php
+
+if (isset($_POST['barnabe'])) {
+    $mail = isset($_POST['mail']) ? $_POST['mail'] : "";
+    $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : "";
+
+    $erreurs = array();
+
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+        array_push($erreurs, "Veuillez saisir une adresse mail valide.");
+
+    if (mb_strlen($mdp) < 6)
+        array_push($erreurs, "Votre mot de passe doit comporter 6 caractères minimum");
+
+    if (count($erreurs) > 0) {
+        $message = "<ul>";
+        $i = 0;
+
+        while ($i < count($erreurs)) {
+            $message .= "<li>" . $erreurs[$i] . "</li>";
+            $i++;
+        }
+
+        $message .= "</ul>";
+
+        echo $message;
+
+        include "frmLogin.php";
+    } else {
+        echo "Test matching login/password";
+
+        //$sql = "SELECT COUNT(*) FROM t_users WHERE USEMAIL='". $mail . "'";
+        //$nombreOccurences = $pdo->query($sql)->fetchColumn();
+
+
+    }
+
+} else {
+    require_once "frmLogin.php";
+}
