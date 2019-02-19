b0cbbd582586568fa58b9ebf92b4f98900220769
Frédéric NOËL <contact@fredericnoel.com>
2019-02-19 12:21:13 +0100

    Test affichage login/logout

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,15 @@
             <li><a href="index.php?page=accueil">Accueil</a></li>
             <li><a href="index.php?page=produits">Produits</a></li>
             <li><a href="index.php?page=inscription">Inscription</a></li>
-            <li><a href="index.php?page=login">Login</a></li>
+            <?php
+            if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+                echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+            }
+
+            else {
+                echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+            }
+            ?>
             <li><a href="index.php?page=contact">Contact</a></li>
         </ul>
     </nav>
